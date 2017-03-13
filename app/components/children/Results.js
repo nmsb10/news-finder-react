128509ac3ea47e830f14dbb222d19877893d529a
nmsb10 <j-nagatani@u.northwestern.edu>
2017-03-13 15:17:37 -0500

    save button appears

--- app/components/children/Results.js
+++ app/components/children/Results.js
@@ -5,29 +5,83 @@ var React = require("react");
 var Results = React.createClass({
 	getInitialState: function(){
 		return{
-			articlesReceived: this.props.articlesFound
+			articlesReceived: this.props.articlesFound,
+			saveCode: ''
 		};
 	},
 	//if the component changes (ie if new articles are found)
 	componentDidUpdate: function(){
 		console.log('Results.js: ', this.state.articlesReceived);
 	},
+	handleChangeSaveCode: function(event){
+		this.setState({saveCode: event.target.value});
+	},
+	handleSubmitSave: function(event){
+		event.preventDefault();
+		console.log('save this article');
+		// if(this.state.searchTerm === ''){
+		// 	this.setState({searchPlaceholder: 'please first enter a topic to search'});
+		// }else{
+		// 	this.props.sendSearchTerms(this.state.searchTerm, this.state.numberOfRecords, this.state.startYear, this.state.endYear);
+		// 	this.setState({
+		// 		searchTerm: '',
+		// 		numberOfRecords: 5,
+		// 		startYear: '',
+		// 		endYear: '',
+		// 		searchPlaceholder: 'enter another topic to search!'
+		// 	});
+		// }
+	},
 	render: function(){
 		return (
 			<div className="panel panel-primary">
 				{/*Panel Heading for the retrieved articles box*/}
 				<div className="panel-heading">
-					<h3 className="panel-title"><strong><i className="fa fa-table"></i> Top Articles</strong></h3>
+					<h3 className="panel-title"><strong><i className="fa fa-table"></i> Top Articles Found</strong></h3>
 				</div>
 				{/*This main panel will hold each of the resulting articles*/}
 				<div className="panel-body" id="wellSection">
 					{this.props.articlesFound.map(function(search, i) {
 						return (
-							<p key={i}>{search._id}<br/>
-							search headline: {search.headline.main}<br/>
-							abstract: {search.lead_paragraph}<br/> {/*or snippet?*/}
-							publication date: {search.pub_date}<br/>
-							web url: {search.web_url}</p>
+							<div key = {i} className = 'panel panel-success'>
+								<div className = 'panel-heading'>
+									<h3>{search.headline.main}</h3>
+								</div>
+								<div className = 'panel-body'>
+									<h5>{search.byline.original}</h5>
+									<h5>{search.lead_paragraph}</h5>{/*or snippet?*/}
+									<h5>{search.pub_date}</h5>
+									<p>{search._id}</p>
+									<a href={search.web_url}>{search.web_url}</a>
+									<form role = 'form' className = 'clearfix'>
+										<div className="form-group pull-left">
+											<input
+												type="text" 
+												className="form-control" 
+											/>
+											<label>enter your save password above</label>
+										</div>
+										<button type = 'submit' className="btn btn-default pull-right">Save Article</button>
+									</form>
+								</div>
+								{/* run this component's handleSubmit function when the form is submitted per the submit button*/}
+								{/*
+								<form role="form" onSubmit = {this.handleSubmitSave}>
+									{/*input for capturing the search term
+									<div className="form-group">
+										<label>save password:</label>
+										<input
+											type="text" 
+											className="form-control" 
+											value = {this.state.saveCode} 
+											onChange = {this.handleChangeSaveCode}
+										/>
+									</div>
+									{/*input for capturing the parameter saveordelete code
+									<button type="submit" className="btn btn-default">Save Article</button>
+								</form>
+								*/}
+							</div>
 						);
 					})}
 				</div>
